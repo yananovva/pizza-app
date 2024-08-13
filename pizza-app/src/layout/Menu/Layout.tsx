315fe055d0ca6007a7afe773c91b5e6fdc8b7ee0
Yana Uranova <uranovayr@gmail.com>
2024-08-13 09:38:07 +0300

    add headling and search

--- pizza-app/src/layout/Menu/Layout.tsx
+++ pizza-app/src/layout/Menu/Layout.tsx
@@ -1,30 +1,38 @@
-import {Link, Outlet} from "react-router-dom";
+import {NavLink, Outlet} from "react-router-dom";
 import styles from './Layout.module.css';
 import Button from "../../components/Button/Button.tsx";
+import cn from "classnames";
 
 export function Layout() {
-    return <div className={styles['layout']}>
-        <div className={styles['sidebar']}>
-            <div className={styles['user']}>
-                <img className={styles['avatar']} src="/avatar-icon.svg" alt="Аватар пользователя"/>
-                <div className={styles['name']}>Марина Мельтон</div>
-                <div className={styles['email']}>m.melton@gmail.com</div>
+
+    return <>
+        <div className={styles['layout']}>
+            <div className={styles['sidebar']}>
+                <div className={styles['user']}>
+                    <img className={styles['avatar']} src="/avatar-icon.svg" alt="Аватар пользователя"/>
+                    <div className={styles['name']}>Марина Мельтон</div>
+                    <div className={styles['email']}>m.melton@gmail.com</div>
+                </div>
+                <div className={styles['menu']}>
+                    <NavLink to='/' className={({isActive}) => cn(styles['link'], {
+                        [styles.active]: isActive
+                    })}>
+                        <img src="/icon-menu-1.svg" alt="Иконка меню"/>
+                        Меню</NavLink>
+                    <NavLink to='/cart' {({isActive}) => cn(styles['link'], {
+                        [styles.active]: isActive
+                    })}>
+                        <img src="/icon-menu-2.svg" alt="Иконка корзины"/>
+                        Корзина</NavLink>
+                </div>
+                <Button className={styles['exit']}>
+                    <img src="/exit-icon.svg" alt="Иконка выхода"/>
+                    Выход
+                </Button>
             </div>
-            <div className={styles['menu']}>
-                <Link to='/' className={styles['link']}>
-                    <img src="/icon-menu-1.svg" alt="Иконка меню"/>
-                    Меню</Link>
-                <Link to='/cart' className={styles['link']}>
-                    <img src="/icon-menu-2.svg" alt="Иконка корзины"/>
-                    Корзина</Link>
+            <div className={styles['content']}>
+                <Outlet/>
             </div>
-            <Button className={styles['exit']}>
-                <img src="/exit-icon.svg" alt="Иконка выхода"/>
-                Выход
-            </Button>
-        </div>
-        <div>
-            <Outlet/>
-        </div>
-    </div>;
+        </div>;
+    </>
 }
--- pizza-app/src/pages/Menu/Menu.tsx
+++ pizza-app/src/pages/Menu/Menu.tsx
@@ -1,5 +1,12 @@
-
+import Headling from "../../components/Headling/Headling.tsx";
+import Search from "../../components/Search/Search.tsx";
+import styles from './Menu.module.css'
 
 export function Menu() {
-    return <>Menu</>;
+    return <>
+        <div className={<styles['head']}>
+        <Headling>Меню</Headling>
+        <Search placeholder='Введите блюдо или состав'/>
+        </div>
+    </>;
 }
